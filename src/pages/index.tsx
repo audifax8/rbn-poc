a0502bae7d83f45a3fa775d6e89e7b67b1488817
Javier Gómez <franciscog@thisisyr.com>
2025-02-28 09:23:02 -0500

    Delete CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import { useSearchParams } from 'next/navigation';
 
 import { IAppState, RenderType } from '@/constants';
 
-import Scripts from '../scritps';
-import style from '../css/app.module.css';
 import Header from '@/components/header';
 import Model from '@/components/model';
 import Loader from '@/components/loader';
 import RenderMenu from '@/components/render-menu';
 import Menu from '@/components/menu';
+
+import Scripts from '../scritps';
+import style from './index.module.css';
 
 export default function App() {
   const searchParams = useSearchParams();
